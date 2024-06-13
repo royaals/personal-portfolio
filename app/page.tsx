378427f6572d8034d31ac306129574b8759e7116
Royal <royal.21ai040@sode-edu.in>
2024-06-13 23:00:30 +0530

    feat: add my approach section

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
-import Approach from "@/components/Aproach";
+import Approach from "@/components/Approach";
 
 
 const Home = () => {
