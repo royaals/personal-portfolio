13a435abd31b4291d3cb89e286c35a1603d7d255
Royal <royal.21ai040@sode-edu.in>
2024-06-16 11:42:48 +0530

    feat: change hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,26 @@
+//@ts-nocheck
 import { FiDownload } from "react-icons/fi";
 import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import Link from "next/link";
 const Hero = () => {
+  const openAndDownloadResume = (url, filename) => {
+    
+    const newTab = window.open(url, '_blank');
+    if (newTab) {
+      newTab.focus();
+    }
+  
+   
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <div className="pb-20 pt-36">
      
@@ -23,9 +40,9 @@ const Hero = () => {
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
-     
+       
         <div
-         
+          
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
         />
@@ -33,22 +50,31 @@ const Hero = () => {
 
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-        
-
-          
-          <TextGenerateEffect
+        <TextGenerateEffect
             words="Hi! I&apos;m Royal, a Full Stack Developer"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
-
-        
-          <Link href="resume.pdf" download="royal.pdf">
-          <MagicButton
-              title="Resume"
-              icon={<FiDownload />}
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-xl">
+          As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects, showcasing my expertise in web development.
+          </p>
+          <div className="flex gap-3">
+          <a href="#projects">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
               position="right"
             />
-          </Link>
+          </a>
+          <a onClick={() => openAndDownloadResume('resume.pdf', 'royal.pdf')} style={{ cursor: 'pointer' }}>
+        <MagicButton
+          title="Resume"
+          icon={<FiDownload />}
+          position="right"
+        />
+      </a>
+
+          </div>
+          
           
         
         </div>
